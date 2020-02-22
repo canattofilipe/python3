abb42f6cfb1a71b65e544400c2149b8f8a435012
Filipe Canatto <canatto.filipe@gmail.com>
2020-02-22 19:56:58 -0300

    first commit

---
+++ fundamentos/fundamentos.py
@@ -0,0 +1,8 @@
+# primeiros exemplos.
+print('primeiro programa')
+
+# instrucoes com 2 linhas.
+sum = 1 \
+    + 2
+
+print(sum)
---
+++ fundamentos/tipos_basicos.py
@@ -0,0 +1,23 @@
+# tipos basicos com exemplos.
+
+# boolean
+print(True)
+print(False)
+
+# float
+print(1.2 + 1)
+
+# string
+print('hello world')
+print('voce é ' + 3 * 'muito '+'legal!')
+# print(3 + '3') -> ambiguidade
+
+# lista
+print([10, 20, 30])
+
+# dicionario
+my_dcit = {'nome': 'lipe', 'idade': 26, 'profissao': 'programador'}
+print(my_dcit['nome'])
+
+# null
+print(None)
