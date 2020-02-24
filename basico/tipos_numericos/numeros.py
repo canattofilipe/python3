e3d70ba2a116d0515d1aad1d61441cee40d5add6
Filipe Canatto <canatto.filipe@gmail.com>
2020-02-24 10:37:09 -0300

    aulas basicas sobre python3

---
+++ basico/tipos_numericos/numeros.py
@@ -0,0 +1,37 @@
+print(dir(int))
+print(dir(float))
+
+a = 5
+b = 2.5
+print(a / b)
+print(a + b)
+print(a * b)
+
+print(type(a))
+print(type(b))
+print(type(a - b))
+
+print(b.is_integer())  # False
+print(5.0.is_integer())  # True
+
+# operando com funcoes dentro do modulo int
+print(int.__add__(2, 3))
+print((-2).__abs__())
+
+# operando com builtin function.
+print(abs(-2))
+
+"""
+A funcao abs() referencia/mapeia a funcao int.__abs__()
+"""
+
+# operando com funcoes dentro do modulo float
+print(float.__add__(2.5, 3))
+print((-2.9).__abs__())
+
+# operando com builtin function.
+print(abs(-2.9))
+
+"""
+A funcao abs() referencia/mapeia a funcao float.__abs__()
+"""
