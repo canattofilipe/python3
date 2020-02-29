d95d382e1a8c40d95089aa39f7311651a216406d
Filipe Canatto <canatto.filipe@gmail.com>
2020-02-29 09:50:32 -0300

    aulas basicas sobre python3

---
+++ basico-projeto/area_circulo_v2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Mais detalhes sobre shebang lines em:
+https://docs.python.org/3/using/windows.html#shebang-lines
+"""
+
+"""
+Calcula a area do circulo.
+"""
+
+PI = 3.14159
+raio = 15.3
+
+print('Área do circulo =', PI * raio ** 2)
