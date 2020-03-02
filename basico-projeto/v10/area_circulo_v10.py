699135ff79fa315d1194d9db59e7ccee36c46532
Filipe Canatto <canatto.filipe@gmail.com>
2020-03-02 20:43:37 -0300

    aulas basicas sobre python3

---
+++ basico-projeto/v10/area_circulo_v10.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# import math
+from math import pi
+import sys
+
+"""
+Calcula a area do circulo.
+"""
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("""\
+É necessário informar o raio do círculo.
+Sintaxe: """, sys.argv[0], '<raio>')
+    else:
+        print(sys.argv[0])  # uri do script.
+        print(sys.argv[1])  # uri do script.
+        area = circulo(sys.argv[1])
+        print('Área do círculo', area)
