86805545d1f0320b42a16d563ed60031217aeec3
Filipe Canatto <canatto.filipe@gmail.com>
2020-03-03 20:19:12 -0300

    aulas basicas sobre python3

---
+++ basico-projeto/v13/README.md
@@ -0,0 +1,25 @@
+Programa que calcula a área do circulo.
+
+Usa shebang lines para chamar o script via terminal sem invocar o interpretador.
+
+Importa o módulo "math" para usar o valor de "pi" que esta contido nesse módulo.
+
+Recebe o valor de "pi" através de entrada do usuário.
+
+Imprimi o nome do módulo.
+
+Condiciona a execução do programa ao nome do módulo.
+
+Isola o calculo da circunferência em uma função.
+
+Altera a função de calculo para retornar valor.
+
+Passa valor para função usando dados vindos do terminal.
+
+Valida os argumentos.
+
+Criando uma função help.
+
+Retorna um erro para o sistema operacional.
+
+Valida se o argumento passado para o programa é do tipo númerico.
---
+++ basico-projeto/v13/area_circulo_v13.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# import math
+from math import pi
+import sys
+import errno
+
+"""
+Calcula a area do circulo.
+"""
+
+
+def help():
+    print("""\
+É necessário informar o raio do círculo.
+Sintaxe: {} <raio>""".format(sys.argv[0][2:]))
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        sys.exit(errno.EPERM)
+
+    if not sys.argv[1].isnumeric():
+        help()
+        print('O raio deve ser um valor númerico')
+        sys.exit(errno.EINVAL)
+
+    print(sys.argv[0])  # uri do script.
+    print(sys.argv[1])  # uri do script.
+    area = circulo(sys.argv[1])
+    print('Área do círculo', area)
