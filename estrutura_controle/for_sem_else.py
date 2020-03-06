bb89e6ae809af742ae717e30995f589d716b18c8
wcanatto <wcanatto@cpqd.com.br>
2020-03-06 10:00:28 -0300

    aulas basicas sobre python

---
+++ estrutura_controle/for_sem_else.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+PALAVRAS_PROIBIDAS = ('futebol', 'religião', 'politica')
+textos = [
+    'João gosta de futebol e politica',
+    'A praia foi divertida'
+]
+
+for texto in textos:
+    found = False
+    for palavra in texto.lower().split():
+        if palavra in PALAVRAS_PROIBIDAS:
+            print('Texto possui pelo menos uma palavra proibida')
+            found = True
+            break
+
+    if not found:
+        print('Texto autorizado', texto)
