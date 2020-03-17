d75c70ef009deec5b0a914b9421de0a1ffd1150e
canattofilipe <canatto.filipe@gmail.com>
2020-03-17 16:56:16 -0300

    aulas basicas sobre python

---
+++ funcoes/prog_funcional_spoiler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def executar(funcao):
+    if (callable(funcao)):
+        funcao()
+
+
+def bom_dia():
+    print('bom dia!')
+
+
+def boa_tarde():
+    print('boa tarde!')
+
+
+if __name__ == '__main__':
+    executar(bom_dia)
+    executar(boa_tarde)
+    executar(1)
