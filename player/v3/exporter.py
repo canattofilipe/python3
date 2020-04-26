60d20c299567556393f765341e827bb6b29ddbc3
Filipe Canatto <canatto.filipe@gmail.com>
2020-04-25 23:07:56 -0300

    ci01

--- player/v3/exporter.py
+++ player/v3/exporter.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
 
 from prettytable import PrettyTable
-from prettytable import MSWORD_FRIENDLY
-x = PrettyTable()
+import jinja2
+
 x1 = PrettyTable()
-
-x1.set_style(MSWORD_FRIENDLY)
-
-
-x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-x.add_row(["Adelaide", 1295, 1158259, 600.5])
-x.add_row(["Brisbane", 5905, 1857594, 1146.4])
-x.add_row(["Darwin", 112, 120900, 1714.7])
-x.add_row(["Hobart", 1357, 205556, 619.5])
-x.add_row(["Sydney", 2058, 4336374, 1214.8])
-x.add_row(["Melbourne", 1566, 3806092, 646.9])
-x.add_row(["Perth", 5386, 1554769, 869.4])
-
-
-#mystring = x.get_html_string()
-# print(mystring)
 
 
 def export(l):
@@ -31,4 +15,11 @@ def export(l):
         avg = sum(value)/len(value)
         avg = "{:.2f}".format(avg).replace('.', ',')
         x1.add_column(key, [min, max, avg])
-    print(x1.get_html_string(format=True))
+    table = x1.get_html_string(attributes={"id": "customers"})
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "template.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    outputText = template.render(table=table)
+    print(outputText)
