b62b1cf0f590eaf09675a703cce7c360339f4aa2
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-18 20:35:39 -0300

    desafio carro

--- poo/data_v1.py
+++ poo/data_v1.py
@@ -2,6 +2,12 @@
 
 
 class Data:
+
+    def __init__(self, dia=1, mes=1, ano=1970):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
     # my function impl.
     def to_str(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
@@ -11,14 +17,8 @@ class Data:
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
-d1 = Data()
-d1.dia = 5
-d1.mes = 12
-d1.ano = 2019
+d1 = Data(5, 12, 2013)
 print(d1.to_str())
 
-d2 = Data()
-d2.dia = 7
-d2.mes = 11
-d2.ano = 2020
+d2 = Data(7, 11, 2020)
 print(d2)
