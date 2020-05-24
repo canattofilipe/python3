8053f42b83ba949d1bff2042f19de6c39caa10a8
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-24 11:09:36 -0300

    classe projeto

---
+++ poo/todo_v2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+from datetime import datetime
+
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def add(self, descricao):
+        self.tarefas.append(Tarefa(descricao))
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao][0]
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())}) tarefa(s) pendente(s))'
+
+
+class Tarefa:
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + (' (Concluída)' if self.feito else '')
+
+
+def main():
+    casa = Projeto('Tarefas de Casa')
+    casa.add('Passar Roupa')
+    casa.add('Lavar Roupa')
+
+    casa.procurar('Lavar Roupa').concluir()
+    for t in casa.tarefas:
+        print(f'- {t}')
+
+    print(casa)
+
+
+if __name__ == '__main__':
+    main()
