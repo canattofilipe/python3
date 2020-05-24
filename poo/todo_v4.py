6130340c2198ed1f42a4824559e1c0be987a1cc0
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-24 11:30:35 -0300

    impl. vencimento da tarefa

---
+++ poo/todo_v4.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+from datetime import datetime, timedelta
+
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def __iter__(self):
+        return self.tarefas.__iter__()
+
+    def add(self, descricao, vencimento=None):
+        self.tarefas.append(Tarefa(descricao, vencimento))
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao][0]
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())}) tarefa(s) pendente(s))'
+
+
+class Tarefa:
+    def __init__(self, descricao, vencimento=None):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+        self.vencimento = vencimento
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        status = []
+        if self.feito:
+            status.append('(Concluída)')
+        elif self.vencimento:
+            if datetime.now() > self.vencimento:
+                status.append('(Vencida)')
+            else:
+                dias = (self.vencimento - datetime.now()).days
+                status.append(f'(Vence em {dias} dias)')
+
+        return f'{self.descricao} '+' '.join(status)
+
+
+def main():
+    casa = Projeto('Tarefas de Casa')
+    casa.add('Passar Roupa', datetime.now())
+    casa.add('Lavar Roupa', datetime.now() + timedelta(days=3, minutes=12))
+    casa.add('Lavar Prato', datetime.now() + timedelta(days=3, minutes=12))
+
+    casa.procurar('Lavar Roupa').concluir()
+    for t in casa:
+        print(f'- {t}')
+
+    print(casa)
+
+
+if __name__ == '__main__':
+    main()
