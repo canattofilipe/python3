e7748e956259e02e2dd08cf0f0edcd906ac253a6
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-26 10:21:18 -0300

    Membros de classe Vs Membros de instancia.

---
+++ poo/poo_avancada/evolucao_v1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+Membros de classe Vs Membros de instancia.
+'''
+
+
+class Humano:
+    # atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+
+if __name__ == '__main__':
+    jose = Humano('José')
+    grokn = Humano('Grokn').das_cavernas()
+
+    print(f'Humano.especice: {Humano.especie}')
+    print(f'jose.especie: {jose.especie}')
+    print(f'grokn.especie: {grokn.especie}')
