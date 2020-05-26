11069d3082ff97cde66c88e307143aca45a9cb5b
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-26 11:09:21 -0300

    Classes abstratas #2

---
+++ poo/poo_avancada/evolucao_v6.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+from abc import ABCMeta, abstractproperty
+
+'''
+Classes abstratas #2
+'''
+
+
+class Humano(metaclass=ABCMeta):
+    # atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+        # _ indica attr privado por convencao.
+        self._idade = None
+
+    @abstractproperty
+    def inteligente(self):
+        pass
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um número positivo!')
+        self._idade = idade
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+    @property
+    def inteligente(self):
+        return False
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+    @property
+    def inteligente(self):
+        return True
+
+
+if __name__ == '__main__':
+
+    try:
+        anonimo = Humano('John Dow')
+        print(anonimo.inteligente)
+    except TypeError:
+        print('Classe abstrata')
+
+    jose = HomoSapiens('José')
+    print('{} da classe {}, inteligente: {}'.
+          format(jose.nome, jose.__class__.__name__, jose.inteligente))
+
+    grokn = Neanderthal('Grokn')
+    print('{} da classe {}, inteligente: {}'.
+          format(grokn.nome, grokn.__class__.__name__, grokn.inteligente))
