2b60bd172009ffd978c158df8cfa13001fa3cbb7
Filipe Canatto <canatto.filipe@gmail.com>
2020-05-31 15:11:57 -0300

    funcoes de primeira classe

---
+++ programacao_funcional/funcao_primeira_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def dobro(x):
+    return x * 2
+
+
+def quadrado(x):
+    return x ** 2
+
+
+if __name__ == '__main__':
+    d = dobro
+    print(d(5))
+    # Retorar alternadamente o dobro ou quadrado nos números 1 a 10
+    funcs = [dobro, quadrado] * 5
+    for func, numero in zip(funcs, range(1, 11)):
+        print(f'O {func.__name__} de {numero} é {func(numero)}')
