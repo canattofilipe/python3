a3e3666f217be434316a3fad94aacf4756db784b
Filipe Canatto <canatto.filipe@gmail.com>
2020-06-23 10:11:43 -0300

    listar bancos

---
+++ banco_dados/listar_bancos.py
@@ -0,0 +1,16 @@
+#!python3
+
+from mysql.connector import connect
+
+conexao = connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='cpqdcpqd'
+)
+
+cursor = conexao.cursor()
+cursor.execute('SHOW DATABASES')
+
+for i, database in enumerate(cursor, start=1):
+    print(f'Banco de dados {i}: {database[0]}')
