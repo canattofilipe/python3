7aa080a644478043b3a5bb69efffce4ff7d3074b
Filipe Canatto <canatto.filipe@gmail.com>
2020-06-25 10:18:43 -0300

    alterar tabela

---
+++ banco_dados/alterar_tabela.py
@@ -0,0 +1,14 @@
+#!python3
+
+from db import nova_conexao
+from mysql.connector import ProgrammingError
+
+sql = 'ALTER TABLE contatos ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY'
+
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+    except ProgrammingError as e:
+        print(f'Erro  : {e.msg}')
