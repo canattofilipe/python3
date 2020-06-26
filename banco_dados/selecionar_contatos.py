6e37f3fbdd677983094da64e53274434a54c1617
Filipe Canatto <canatto.filipe@gmail.com>
2020-06-26 10:52:09 -0300

    consultar registros

---
+++ banco_dados/selecionar_contatos.py
@@ -0,0 +1,17 @@
+#!python3
+
+from db import nova_conexao
+from mysql.connector import ProgrammingError
+
+sql = 'SELECT * FROM contatos'
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        contatos = cursor.fetchall()
+    except ProgrammingError as e:
+        print(f'Erro  : {e.msg}')
+    else:
+        for contato in contatos:
+            print(f'{contato[2]:2d} = {contato[0]:10s} Telefone: {contato[1]}')
