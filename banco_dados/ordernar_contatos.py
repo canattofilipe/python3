4cb0ae24886c3e772232674ad0c7583501565177
Filipe Canatto <canatto.filipe@gmail.com>
2020-06-29 20:35:38 -0300

    ordernar contatos

---
+++ banco_dados/ordernar_contatos.py
@@ -0,0 +1,12 @@
+#!python3
+
+from db import nova_conexao
+
+sql = "SELECT nome FROM contatos ORDER BY nome DESC"
+
+with nova_conexao() as conexao:
+
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+
+    print('\n'.join(registro[0] for registro in cursor))
